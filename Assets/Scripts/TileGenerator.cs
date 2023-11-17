d623f5450eec05f4ba5a342849cabee52c4205ee
sinyana383 <sinyana383@gmail.com>
2023-11-17 17:52:41 +0300

    Generator fixed

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,47 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TileGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] tilesPrefabs;
-    [SerializeField] private List<GameObject> presentTiles = new List<GameObject>();
+    [SerializeField] private GameObject groundPrefab;
+    [SerializeField] private List<GameObject> presentGrounds = new List<GameObject>();
+    [SerializeField] private float tilesInGround = 50;
+    [SerializeField] private int onStartGrounds = 6;
     private float spawnPos = 0;
+    
+    [SerializeField] private GameObject[] tilesPrefabs;
     // TODO: Auto-recognize length
-    [SerializeField] private float tileLength = 100;
-    [SerializeField] private int onStartTiles = 3;
+    [SerializeField] private float tileLength = 10;
+    
 
     [SerializeField] private Transform player;
     void Start()
     {
-        for (int i = 0; i < onStartTiles; ++i)
+        for (int i = 0; i < onStartGrounds; ++i)
         {
-            SpawnTile(Random.Range(0, tilesPrefabs.Length));
+            SpawnGround(Random.Range(0, tilesPrefabs.Length));
         }
     }
     
     void Update()
     {
-        if (player.position.z - tileLength > spawnPos - (onStartTiles * tileLength))
+        if (player.position.z - tilesInGround * tileLength > spawnPos - (onStartGrounds * tilesInGround * tileLength))
         {
-            SpawnTile(Random.Range(0, tilesPrefabs.Length));
+            SpawnGround(Random.Range(0, tilesPrefabs.Length));
             DeleteAndDestroyTile();
         }
 
     }
 
     // TODO: make a cycle run
-    private void SpawnTile(int tileIndex)
+    private void SpawnGround(int tileIndex)
     {
-        GameObject newTile = Instantiate(tilesPrefabs[tileIndex], transform.forward * spawnPos, transform.rotation);
-        presentTiles.Add(newTile);
+        GameObject newGround = Instantiate(groundPrefab, Vector3.forward * spawnPos, transform.rotation);
+        for (int i = 0; i < tilesInGround; i++)
+        {
+            var tilePosition = Vector3.forward * spawnPos + new Vector3(0, 0, i * tileLength);
+            GameObject newTile = Instantiate(tilesPrefabs[tileIndex], tilePosition, transform.rotation);
+            newTile.transform.SetParent(newGround.transform);
+        }
+        presentGrounds.Add(newGround);
         
-        spawnPos += tileLength;
+        spawnPos += tilesInGround * tileLength;
     }
 
     private void DeleteAndDestroyTile()
     {
-        Destroy(presentTiles[0]);
-        presentTiles.RemoveAt(0);
+        Destroy(presentGrounds[0]);
+        presentGrounds.RemoveAt(0);
     }
 }
