f6a6210251fe5071c80184b483609bf98ca99a00
sinyana383 <sinyana383@gmail.com>
2023-11-18 02:40:58 +0300

    tiles in row generate

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -13,12 +13,13 @@ public class TileGenerator : MonoBehaviour
     
     [SerializeField] private GameObject[] tilesPrefabs;
     // TODO: Auto-recognize length
-    [SerializeField] private float tileLength = 10;
-    
+    [SerializeField] private float tileLength;
 
     [SerializeField] private Transform player;
     void Start()
     {
+        tileLength = tilesPrefabs[0].transform.localScale.z;
+        
         spawnPos = -(tilesInGround * tileLength) / 2;
         for (int i = 0; i < onStartGrounds; ++i)
         {
@@ -43,12 +44,26 @@ public class TileGenerator : MonoBehaviour
         for (int i = 0; i < tilesInGround; i++)
         {
             var tilePosition = Vector3.forward * spawnPos + new Vector3(0, 0, i * tileLength);
-            GameObject newTile = Instantiate(tilesPrefabs[tileIndex], tilePosition, transform.rotation);
+            GameObject newTile = SpawnRaw(tilePosition.z, tileIndex);
             newTile.transform.SetParent(newGround.transform);
         }
         presentGrounds.Add(newGround);
         
         spawnPos += tilesInGround * tileLength;
+    }
+
+    private GameObject SpawnRaw(float posZ, int tileIndex)
+    {
+        var row = new GameObject();
+        
+        row.transform.position = new Vector3(0f, 0f, posZ);
+        row.transform.rotation = transform.rotation;
+
+        GameObject middleTile = Instantiate(tilesPrefabs[tileIndex], new Vector3(0, 0, posZ), transform.rotation, row.transform);
+        GameObject leftTile = Instantiate(tilesPrefabs[tileIndex], new Vector3(-tileLength, 0, posZ), transform.rotation, row.transform);
+        GameObject rightTile = Instantiate(tilesPrefabs[tileIndex], new Vector3(tileLength, 0, posZ), transform.rotation, row.transform);
+        
+        return row;
     }
 
     private void DeleteAndDestroyTile()
