58e89f5972bd398c38c70e715d63ccdd05344250
sinyana383 <sinyana383@gmail.com>
2023-11-18 16:32:23 +0300

    show Flames on start

--- Assets/Scripts/UI/LifeManager.cs
+++ Assets/Scripts/UI/LifeManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Image[] fires;
+    [SerializeField] private Sprite noFire;
+    [SerializeField] private Sprite fire;
+    
     void Start()
     {
-        
+        for (int i = 0; i < fires.Length / 2; i++)
+        {
+            fires[i].sprite = fire;
+        }
     }
 
     // Update is called once per frame
